5156f6a42a9118cef3589c51991e17452f4c90d0
Looking4OffSwitch <lookingfortheoffswitch@gmail.com>
2020-09-19 09:18:33 -0400

    General cleanup and add extra documentation

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import pytz
 import random
 
-from time_digits import TimeDigits, TIME_SEGMENTS, DIGIT_TO_SEGMENT_STATE
+from time_digits import TimeDigits, TIME_SEGMENTS, DIGIT_TO_SEGMENTS_MAP
 
 from rpi_ws281x import *
 
@@ -15,7 +15,7 @@ LED_COUNT      = LEDS_PER_SEGMENT * TOTAL_SEGMENTS # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM! GPIO 13 and 18 on RPi 3).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (Between 1 and 14)
-LED_BRIGHTNESS = 50     # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 200     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # 0 or 1
 LED_STRIP      = ws.WS2812_STRIP
@@ -24,36 +24,21 @@ LED_STRIP      = ws.WS2812_STRIP
 class LEDClock():
 
     _BLACK_COLOR = Color(0, 0, 0)
-    _SECOND_INDICATOR_COLOR = Color(10, 10, 10)
+    _SECOND_INDICATOR_COLOR = Color(30, 30, 30)
 
     def __init__(self, strip, time_zone='America/New_York'):
         self.strip = strip
         self.clear()
 
-        # blinks on and off each second seconds
+        # blinks on and off each second
         self.second_indicator_segment = 20
         self.second_indicator_is_on = False
 
         self.cur_timezone = pytz.timezone(time_zone)
 
-        self.hour1 = None
-        self.hour2 = None
-        self.min1 = None
-        self.min2 = None
+        self.hour1 = self.hour2 = self.min1 = self.min2 = None
 
-    def second_indicator_pulse(self):
-        if self.second_indicator_is_on:
-            # Turn it off
-            strip.setPixelColor(middle_led_index, self._BLACK_COLOR)
-            self.second_indicator_is_on = False
-        else:
-            # Turn it on
-            strip.setPixelColor(middle_led_index, self._SECOND_INDICATOR_COLOR)
-            self.second_indicator_is_on = True
-
-        self.strip.show()
-
-    def updated_second_indicator(self):
+    def update_second_indicator(self):
         if self.second_indicator_is_on:
             # Turn it off
             self.clear_segment(self.second_indicator_segment, True)
@@ -62,13 +47,10 @@ class LEDClock():
             # Turn it on
             self.light_segment(self.second_indicator_segment, self._SECOND_INDICATOR_COLOR, True)
             self.second_indicator_is_on = True
+        self.strip.show()
 
     def show_current_time(self):
-        # zero padded, 12 hr format (e.g. 10:50 or 04:32)
-        dt_str = datetime.now(self.cur_timezone).strftime('%I:%M')
-        print(dt_str)
-
-        hour1, hour2, min1, min2 = self._split_time(dt_str)
+        hour1, hour2, min1, min2 = self._get_cur_time_digits()
         print(hour1, hour2, min1, min2)
 
         if self.hour1 != hour1:
@@ -94,8 +76,11 @@ class LEDClock():
             self._clear_digit(TimeDigits.MINUTE_TWO)
             self._show_time_helper(TimeDigits.MINUTE_TWO, min2, Color(0, 0, 220))
 
-    def _split_time(self, dt_str):
-        # dt_str: 12 hour time format: ex: '12:49' or '05:12'
+    def _get_cur_time_digits(self) -> (int, int, int, int):
+        # zero padded, 12 hr format (e.g. 10:50 or 04:32)
+        dt_str = datetime.now(self.cur_timezone).strftime('%I:%M')
+        # print(dt_str)
+
         hr1 = int(dt_str[0])
         hr2 = int(dt_str[1])
 
@@ -135,7 +120,7 @@ class LEDClock():
         return start_index
 
     def _show_time_helper(self, timeDigit: TimeDigits, digit: int, color: Color):
-        on_off_flags = DIGIT_TO_SEGMENT_STATE[digit]
+        on_off_flags = DIGIT_TO_SEGMENTS_MAP[digit]
 
         for idx, segment in enumerate(TIME_SEGMENTS[timeDigit]):
             if segment == 0:
@@ -157,8 +142,8 @@ if __name__ == '__main__':
     try:
         while True:
             clock.show_current_time()
-            clock.updated_second_indicator()
+            clock.update_second_indicator()
             time.sleep(1)
 
     except KeyboardInterrupt:
-        clock.clear() 
+        clock.clear()
--- time_digits.py
+++ time_digits.py
@@ -16,7 +16,19 @@ Displaying a 1 would light up segments 1 and 6. Displaying an 8 would light up a
         -------
            5
 
+Layout (and order) of all segements
 
+             7          6        5         4         3         2
+         --------- --------- --------- --------- --------- ---------
+        |         |         |         |         |         |         |
+      8 |         |23       |26       |27       |30       |31       | 1
+        |         |         |         |         |         |         |
+         --------- --------- --------- --------- --------- ---------
+        |    22   |    21   |    20   |    19   |    18   |    17   |
+      9 |         |24       |25       |28       |29       |32       | 16
+        |         |         |         |         |         |         |
+         --------- --------- --------- --------- --------- ---------
+            10         11        12        13        14        15
 """
 
 @unique
@@ -27,13 +39,18 @@ class TimeDigits(Enum):
     MINUTE_ONE = auto() # 12:(5)9
     MINUTE_TWO = auto() # 12:5(9)
 
+# The segments that make up each time digit
 TIME_SEGMENTS = { TimeDigits.HOUR_ONE:   [8,  0,  0,  0,  0,  9,  0],
                   TimeDigits.HOUR_TWO:   [26, 6, 23, 24, 11, 25, 21],
                   TimeDigits.MINUTE_ONE: [30, 4, 27, 28, 13, 29, 19],
                   TimeDigits.MINUTE_TWO: [1,  2, 31, 32, 15, 16, 17]
 }
 
-DIGIT_TO_SEGMENT_STATE = {
+# As shown in TIME_SEGMENTS, each digit is comprised of eight segments. Not all segements
+# are on while a given digit is displayed. This map stores which segments should be lit
+# for a given digit. Each number in a list is used to determine if the segment,
+# corresponding to the TIME_SEGMENTS dict, are on or off.
+DIGIT_TO_SEGMENTS_MAP = {
      # segment number
      #   1, 2, 3, 4, 5, 6, 7
    -1:  [0, 0, 0, 0, 0, 0, 0],  # all segments are off
