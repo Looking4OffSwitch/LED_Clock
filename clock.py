8adac466400105a542b76bc450d25336e7645f12
Looking4OffSwitch <lookingfortheoffswitch@gmail.com>
2020-09-21 10:48:46 -0400

    Major rework of main.py and clock.py to work around memory issues with the LED library

--- clock.py
+++ clock.py
@@ -3,41 +3,17 @@ from datetime import datetime
 import pytz
 
 from time_digits import TimeDigits, TIME_SEGMENTS, DIGIT_TO_SEGMENTS_MAP
-#from downlights import Downlights
 
-from rpi_ws281x import *
-
-# LED strip configuration:
-# LEDS_PER_SEGMENT = 9
-# TOTAL_SEGMENTS   = 32
-#
-# LED_COUNT      = LEDS_PER_SEGMENT * TOTAL_SEGMENTS # Number of LED pixels.
-# LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM! GPIO 13 and 18 on RPi 3).
-# LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-# LED_DMA        = 10      # DMA channel to use for generating signal (Between 1 and 14)
-# LED_BRIGHTNESS = 200     # Set to 0 for darkest and 255 for brightest
-# LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-# LED_CHANNEL    = 0       # 0 or 1
-# LED_STRIP      = ws.WS2812_STRIP
-
+from rpi_ws281x import Adafruit_NeoPixel, Color
 
 class LEDClock():
 
-    _BLACK_COLOR = Color(0, 0, 0)
-    _SECOND_INDICATOR_COLOR = Color(30, 30, 30)
+    def __init__(self, led_strip: Adafruit_NeoPixel, num_segments: int, leds_per_segment:int,
+                 digits_color: Color, second_indicator_color: Color,time_zone='America/New_York'):
 
-    def __init__(self, led_segments: int, leds_per_segment:int, pin: int,
-                 freq: int, dma: int, brightness: int, invert: bool, channel: int,
-                 strip_type: int, time_zone='America/New_York'):
-
-        led_count = led_segments * leds_per_segment
-        self.led_segments = led_segments
+        self.strip = led_strip
+        self.num_segments = num_segments
         self.leds_per_segment = leds_per_segment
-
-        self.strip = Adafruit_NeoPixel(led_count, pin, freq, dma, invert, brightness,
-                                       channel, strip_type)
-        self.strip.begin()
-        self.clear()
 
         # blinks on and off each second
         self.second_indicator_segment = 20
@@ -47,17 +23,28 @@ class LEDClock():
 
         self.hour1 = self.hour2 = self.min1 = self.min2 = None
 
+        self._black_color = Color(0, 0, 0)
+        self._digits_color = digits_color
+        self._second_indicator_color = second_indicator_color
+
     def __del__(self):
+        # Turn off the lights
         self.clear()
+
+    def clear(self):
+        """ Set the entire strip to black (i.e. "off") """
+        for i in range(self.strip.numPixels()):
+            self.strip.setPixelColor(i, self._black_color)
+        self.strip.show()
 
     def update_second_indicator(self):
         if self.second_indicator_is_on:
             # Turn it off
-            self.clear_segment(self.second_indicator_segment, True)
+            self._set_segment_color(self.second_indicator_segment, self._black_color, True)
             self.second_indicator_is_on = False
         else:
             # Turn it on
-            self.light_segment(self.second_indicator_segment, self._SECOND_INDICATOR_COLOR, True)
+            self._set_segment_color(self.second_indicator_segment, self._second_indicator_color, True)
             self.second_indicator_is_on = True
         self.strip.show()
 
@@ -76,22 +63,22 @@ class LEDClock():
             if hour1 == 0:
                 self._clear_digit(TimeDigits.HOUR_ONE)
             else:
-                self._show_time_helper(TimeDigits.HOUR_ONE, hour1, Color(0, 0, 220))
+                self._show_time_helper(TimeDigits.HOUR_ONE, hour1)
 
         if self.hour2 != hour2:
             self.hour2 = hour2
             self._clear_digit(TimeDigits.HOUR_TWO)
-            self._show_time_helper(TimeDigits.HOUR_TWO, hour2, Color(0, 0, 220))
+            self._show_time_helper(TimeDigits.HOUR_TWO, hour2)
 
         if self.min1 != min1:
             self.min1 = min1
             self._clear_digit(TimeDigits.MINUTE_ONE)
-            self._show_time_helper(TimeDigits.MINUTE_ONE, min1, Color(0, 0, 220))
+            self._show_time_helper(TimeDigits.MINUTE_ONE, min1)
 
         if self.min2 != min2:
             self.min2 = min2
             self._clear_digit(TimeDigits.MINUTE_TWO)
-            self._show_time_helper(TimeDigits.MINUTE_TWO, min2, Color(0, 0, 220))
+            self._show_time_helper(TimeDigits.MINUTE_TWO, min2)
 
     def _get_cur_time_digits(self) -> (int, int, int, int):
         # zero padded, 12 hr format (e.g. 10:50 or 04:32)
@@ -106,14 +93,8 @@ class LEDClock():
 
         return (hr1, hr2, min1, min2)
 
-    def clear(self):
-        """ Set the entire strip to black (i.e. "off") """
-        for i in range(self.strip.numPixels()):
-            self.strip.setPixelColor(i, self._BLACK_COLOR)
-        self.strip.show()
-
-    def light_segment(self, segment_num, color = Color(255, 0, 0), show=False):
-        index = self.index_for_segment(segment_num, 0)
+    def _set_segment_color(self, segment_num: int, color: Color, show = False):
+        index = self._index_for_segment(segment_num, 0)
 
         for i in range(index, index + self.leds_per_segment):
             self.strip.setPixelColor(i, color)
@@ -121,23 +102,20 @@ class LEDClock():
         if show:
             self.strip.show()
 
-    def clear_segment(self, segment_num, show=False):
-        self.light_segment(segment_num, self._BLACK_COLOR, show)
-
     def _clear_digit(self, time_digit: TimeDigits):
         for segment in TIME_SEGMENTS[time_digit]:
-            self.clear_segment(segment)
+            self._set_segment_color(segment, self._black_color)
 
-    def index_for_segment(self, segment_num, offset):
+    def _index_for_segment(self, segment_num, offset):
 #TODO error check
-        if segment_num > self.led_segments:
+        if segment_num > self.num_segments:
             # print(f"segments({segment_num}) can not be greater than {TOTAL_SEGMENTS}")
             return None
 
         start_index = segment_num * self.leds_per_segment - self.leds_per_segment
         return start_index
 
-    def _show_time_helper(self, timeDigit: TimeDigits, digit: int, color: Color):
+    def _show_time_helper(self, timeDigit: TimeDigits, digit: int):
         on_off_flags = DIGIT_TO_SEGMENTS_MAP[digit]
 
         for idx, segment in enumerate(TIME_SEGMENTS[timeDigit]):
@@ -145,30 +123,5 @@ class LEDClock():
                 continue
 
             if on_off_flags[idx] == 1:
-                self.light_segment(segment, color, False)
+                self._set_segment_color(segment, self._digits_color, False)
         self.strip.show()
-
-# if __name__ == '__main__':
-#     # Create NeoPixel object
-#     strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-#     strip.begin()
-#
-#     clock = LEDClock(led_segments=32, leds_per_segment=9, pin=13, freq=800000, dma=10, brightness=50,
-#                             invert=False, channel=1, strip_type=ws.WS2812_STRIP)
-#
-#     downlights = Downlights(led_cnt=12, pin=13, freq=800000, dma=10, brightness=50,
-#                             invert=False, channel=1, strip_type=ws.WS2812_STRIP)
-#     downlights.set_all(Color(200, 200, 200))
-#
-#     print ('Press Ctrl-C to quit.')
-#
-#     try:
-#         while True:
-#             clock.show_current_time()
-#             clock.update_second_indicator()
-#             time.sleep(1)
-#
-#     finally:
-#         # This ensures all LEDs are turned off
-#         del clock
-#         del downlights
--- main.py
+++ main.py
@@ -1,15 +1,47 @@
+#!/usr/bin/env python3
+# Author: Reed Mangino
+
 import time
+from rpi_ws281x import *
 from clock import LEDClock
-from downlights import Downlights
-from rpi_ws281x import ws, Color
+
+# LED strip configurations:
+
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA     = 10      # DMA channel to use for generating signal (try 10)
+LED_INVERT  = False   # True to invert the signal (when using NPN transistor level shift)
+
+MAIN_LEDS_PER_SEGMENT   = 9
+MAIN_TOTAL_LED_SEGMENTS = 32
+MAIN_LED_COUNT = MAIN_TOTAL_LED_SEGMENTS * MAIN_LEDS_PER_SEGMENT
+MAIN_LED_PIN            = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+MAIN_LED_BRIGHTNESS     = 255     # Set to 0 for darkest and 255 for brightest
+MAIN_LED_CHANNEL        = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+DOWNLIGHT_LED_COUNT      = 12
+# GPIO pin connected to the pixels (must support PWM! GPIO 13 and 18 on RPi 3).
+DOWNLIGHT_LED_PIN        = 13
+DOWNLIGHT_LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+DOWNLIGHT_LED_CHANNEL    = 1       # 0 or 1
+
+def all_on(strip, color):
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+    strip.show()
 
 def main():
-    clock = LEDClock(led_segments=32, leds_per_segment=9, pin=18, freq=800000, dma=10,
-                     brightness=200, invert=False, channel=0, strip_type=ws.WS2812_STRIP)
+    main_strip = Adafruit_NeoPixel(MAIN_LED_COUNT, MAIN_LED_PIN, LED_FREQ_HZ, LED_DMA,
+                                  LED_INVERT, MAIN_LED_BRIGHTNESS, MAIN_LED_CHANNEL)
+    downlights = Adafruit_NeoPixel(DOWNLIGHT_LED_COUNT, DOWNLIGHT_LED_PIN, LED_FREQ_HZ,
+                                   LED_DMA, LED_INVERT, DOWNLIGHT_LED_BRIGHTNESS, DOWNLIGHT_LED_CHANNEL)
 
-    downlights = Downlights(led_cnt=12, pin=13, freq=800000, dma=10, brightness=50,
-                            invert=False, channel=1, strip_type=ws.WS2812_STRIP)
-    downlights.set_all(Color(128, 128, 128))
+    main_strip.begin()
+    downlights.begin()
+
+    digits_color = Color(0, 0, 255)
+    second_indicator_color = Color(30, 30, 30)
+    clock = LEDClock(main_strip, MAIN_TOTAL_LED_SEGMENTS, MAIN_LEDS_PER_SEGMENT,
+                     digits_color, second_indicator_color)
 
     print ('Press Ctrl-C to quit.')
 
@@ -19,10 +51,14 @@ def main():
             clock.update_second_indicator()
             time.sleep(1)
 
+    except KeyboardInterrupt:
+        pass
+
     finally:
-        # This ensures all LEDs are turned off
-        del clock
-        del downlights
+        # Ensure all downlight LEDs are turned off
+        for i in range(downlights.numPixels()):
+            downlights.setPixelColor(i, Color(0, 0, 0))
+        downlights.show()
 
 if __name__ == '__main__':
     main()
